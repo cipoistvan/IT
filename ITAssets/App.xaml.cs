82cd18f8715097f4f95b3833dfe3186a7e09d647
Istvan Cipo <icipo@hotmail.com>
2025-05-30 23:47:39 +0200

    Beszerzés képernyő, adatbázis kapcsolattal

--- ITAssets/App.xaml.cs
+++ ITAssets/App.xaml.cs
@@ -9,6 +9,12 @@ namespace ITAssets
     /// </summary>
     public partial class App : Application
     {
+        public static string connectionString = "server=localhost;database=itassets;user=root;password=;";
+
+        // public static DatabaseService datasvc = new DatabaseService(connectionString);
+
+
+
     }
 
 }
---
+++ ITAssets/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITAssets
+{
+    public class Purchase
+    {
+
+        public int ID{ get; set; }
+        public DateTime Date { get; set; }
+        public string User { get; set; }
+        public string ItemName { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total => Quantity * UnitPrice;
+    }
+}
