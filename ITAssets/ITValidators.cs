7657fcc4a4a94ca85e609642f434f4d9231d2431
Istvan Cipo <icipo@hotmail.com>
2025-06-16 00:14:18 +0200

    TestUnit készítése

---
+++ ITAssets/ITValidators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ITAssets
+{
+    public sealed class ITValidators
+    {
+        public static bool ValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return false;
+
+            var pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            
+
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+
+
+
+    }
+}
---
+++ ITTest/ITValidatorsTest.cs
@@ -0,0 +1,44 @@
+﻿
+
+using ITAssets;
+
+namespace ITTest
+{
+    [TestClass]
+    public sealed class ITValidatorsTest
+    {
+        [TestMethod]
+        public void ValidEmailCheck()
+        {
+            string emailbad = "aaa";
+            bool expected = false;
+            bool actual = ITValidators.ValidEmail(emailbad);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void ValidEmailCheck2()
+        {
+            string emailbad = "a@b.hu";
+            bool expected = true;
+            bool actual = ITValidators.ValidEmail(emailbad);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void ValidEmailCheck3()
+        {
+            string emailbad = "a@ b.hu";
+            bool expected = false;
+            bool actual = ITValidators.ValidEmail(emailbad);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+
+    }
+}
